96e22548943090eee403d232f47a6b4b959ad46b
spstudysp <spstudysp@gmail.com>
2023-02-25 11:25:43 +0200

    clean env

---
+++ prj_clean_env.py
@@ -0,0 +1,11 @@
+import requests
+
+try:
+    requests.get('http://127.0.0.1:5000/stop_server')
+except Exception as e:
+    print(e)
+
+try:
+    requests.get('http://127.0.0.1:5001/stop_server')
+except Exception as e:
+    print(e)
